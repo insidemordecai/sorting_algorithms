5de536b23da1745dd6e9ca90762fa2675ac002df
Mordecai K <39312305+insidemordecai@users.noreply.github.com>
2023-01-23 09:06:23 +0300

    add: tasks

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include <stddef.h>
+#include "sort.h"
+
+/**
+  * bubble_sort - sorts an array of integers in ascending order
+  * using bubble sort.
+  * @array: array of integers
+  * @size: size of array
+  */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	if (size < 2)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
+/**
+  * swap - swaps two elements
+  * @a: first element
+  * @b: second element
+  */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
